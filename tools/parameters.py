f21523d1c1840e962d25a62d0f36e6646b6527b1
Jorge Jair Ramírez Estrada <jorgejairsmln@hotmail.com>
2014-09-09 19:21:23 -0500

    Changes in parameters

--- tools/parameters.py
+++ tools/parameters.py
@@ -4,5 +4,5 @@ if sys.platform == 'win32':
 	csv_path = 'C:\Users\dasmesser\Documents\ITC\9noSemestre\Sistemas Operativos 2\proyecto\data\mi_registro_matrimonios.csv'
 	output_path = 'C:\Users\dasmesser\Documents\ITC\9noSemestre\Sistemas Operativos 2\proyecto\output'
 else:
-	csv_path = '/home/A01211143/proyecto/data/mi_registro_matrimonios.csv'
-	output_path = '/home/A01211143/proyecto/output'
+	csv_path = '/home/A01211143/projecto/DjangoProject/data/mi_registro_matrimonios.csv'
+	output_path = '/home/A01211143/projecto/DjangoProject/output'
